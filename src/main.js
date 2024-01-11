b76f65dea1e07992e0f3c9055597280a2ae55b48
darkhorsecorder <boryschernenko.dream@gmail.com>
2024-01-11 07:33:06 +0100

    add vue tanstack query and initialize

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import './styles/main.css'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import initializeRouter from './router'
 
 const app = createApp(App)
-
+// Create a new QueryClient instance
+const queryClient = new QueryClient()
+// Create Vue application and use VueQueryPlugin
+app.use(VueQueryPlugin, { queryClient }) // Provide the QueryClient to the Vue app
 initializeRouter(app)
 
 app.mount('#app')
